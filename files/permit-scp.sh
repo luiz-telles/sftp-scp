bb879cfec9adfcb672f49ee8e0539604c0538256
luiz telles <luiz_f_telles@yahoo.com.br>
2024-02-29 14:01:49 +0000

    feat: add scp support
    inspired: https://github.com/jmcombs/sftp

---
+++ files/permit-scp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Permit scp
+#todo: parse parse command  "scp -f" and "scp -t" to force rootpath
+case $SSH_ORIGINAL_COMMAND in
+ 'scp'*)
+    exec $SSH_ORIGINAL_COMMAND
+    ;;
+ '/usr/lib/openssh/sftp-server'*)
+    exec $SSH_ORIGINAL_COMMAND
+    ;;
+   *)
+    echo "Access Denied"
+    ;;
+esac
