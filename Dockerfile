67c8eff205313d38eda1308b762967feb1e757bc
luiz telles <luiz_f_telles@yahoo.com.br>
2024-03-07 12:00:44 +0000

    feat: using /home to chroot

--- Dockerfile
+++ Dockerfile
@@ -11,11 +11,13 @@ RUN apt-get update && \
     mkdir -p /var/run/sshd && \
     rm -f /etc/ssh/ssh_host_*key*
 
-
+COPY files/create-chrootdir.sh /usr/local/bin/
 COPY files/sshd_config /etc/ssh/sshd_config
 COPY files/create-sftp-user /usr/local/bin/
 COPY files/permit-scp.sh /usr/local/bin/
 COPY files/entrypoint /
+
+RUN /usr/local/bin/create-chrootdir.sh
 
 EXPOSE 22
 
---
+++ files/create-chrootdir.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -Eeo pipefail
+
+# shellcheck disable=2154
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+
+chr=/home
+mkdir -p "${chr}"
+chown root:root "${chr}"
+chmod 755 "${chr}"
+
+myBinaries=("sh" "/usr/lib/openssh/sftp-server" "scp" )
+for bin in ${myBinaries[@]}; do
+  pathBin=`which $bin`
+  cp -v --parents "$pathBin" "${chr}"
+  list="$(ldd $pathBin | awk '{if ($2=="=>") print $3 ;else print $1}')"
+  for item in `echo "$list" | sort| uniq`
+  do
+   cp -v --parents "$item" "${chr}" || :
+  done
+done
+
+cp --parents /usr/local/bin/permit-scp.sh ${chr}
+mkdir ${chr}/dev
+mknod ${chr}/dev/null c 1 3
+chmod 666 ${chr}/dev/null
+cp --parents /etc/passwd /lib/x86_64-linux-gnu/libnss_files.so.2  ${chr}/
+
