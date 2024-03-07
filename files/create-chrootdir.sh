99891e142b8ad462d44aadc20d6ec79de475a587
luiz telles <luiz_f_telles@yahoo.com.br>
2024-03-07 13:10:08 +0000

    fix: working for docker alpine

--- files/create-chrootdir.sh
+++ files/create-chrootdir.sh
@@ -10,9 +10,9 @@ mkdir -p "${chr}"
 chown root:root "${chr}"
 chmod 755 "${chr}"
 
-myBinaries=("sh" "/usr/lib/openssh/sftp-server" "scp" )
+myBinaries=("sh" "/usr/lib/openssh/sftp-server" "/usr/lib/ssh/sftp-server" "scp" "ash")
 for bin in ${myBinaries[@]}; do
-  pathBin=`which $bin`
+  pathBin=`which $bin` || continue
   cp -v --parents "$pathBin" "${chr}"
   list="$(ldd $pathBin | awk '{if ($2=="=>") print $3 ;else print $1}')"
   for item in `echo "$list" | sort| uniq`
@@ -21,9 +21,9 @@ for bin in ${myBinaries[@]}; do
   done
 done
 
-cp --parents /usr/local/bin/permit-scp.sh ${chr}
+cp --parents /usr/local/bin/permit-scp.sh ${chr}  
 mkdir ${chr}/dev
 mknod ${chr}/dev/null c 1 3
 chmod 666 ${chr}/dev/null
-cp --parents /etc/passwd /lib/x86_64-linux-gnu/libnss_files.so.2  ${chr}/
+cp --parents /etc/passwd /lib/x86_64-linux-gnu/libnss_files.so.2  ${chr}/ || :
 
---
+++ files/permit-scp-alpine.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Permit scp
+#todo: parse parse command  "scp -f" and "scp -t" to force rootpath
+ 
+case $SSH_ORIGINAL_COMMAND in
+ 'scp'*)
+    exec $SSH_ORIGINAL_COMMAND 
+    ;;
+ '/usr/lib/ssh/sftp-server'*)
+    exec $SSH_ORIGINAL_COMMAND 
+    ;;
+   *)
+    echo "Access Denied"
+    ;;
+esac
